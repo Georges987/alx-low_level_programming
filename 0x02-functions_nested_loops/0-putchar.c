a078141dbf3916e41976b9ca7c928e9c447e9c33
Georges987 <ayenigeorgepierre@gmail.com>
2022-03-15 10:11:12 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * * main - causes an infinite loop
+ * * Return: 0
+ * */
+
+int main(void)
+{
+	char* str="_putchar\n";
+	while(*sh)
+	{
+		_putchar(str);
+	}
+	_putchar('\n');
+	return (0);
+}

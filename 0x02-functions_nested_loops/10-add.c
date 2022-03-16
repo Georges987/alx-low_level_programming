194003e20954378ad65e2133c85847ed45884183
Georges987 <ayenigeorgepierre@gmail.com>
2022-03-16 06:03:14 -0700

    Corrections

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,11 @@
 
 /**
  * add - adds two numbers
- * 
- * a: first parameters
- * 
- * b: second parameters
- * 
+ * @a: first parameters
+ * @b: second parameters
  * Return: the result of sum
  *
  */
-
 int add(int a, int b)
 {
 	return (a + b);

b472df82853e3c2448d181ef5591a4fa163b0e73
Georges987 <ayenigeorgepierre@gmail.com>
2022-03-16 05:53:24 -0700

    Adding guard to if clause statement 9-times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,10 +16,14 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			if (((a * b) / 10) > 0)
+			{
 				_putchar('0' + ((a * b) / 10));
+			}
 			else if (b > 0)
+			{
 				_putchar(' ');
 				_putchar('0' + ((a * b) % 10));
+			}
 			if (b < 9)
 			{
 				_putchar(',');

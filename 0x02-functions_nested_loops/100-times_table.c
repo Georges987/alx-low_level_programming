f4edfa758e54edb8aaeee177fd0b864693bfa486
Georges987 <ayenigeorgepierre@gmail.com>
2022-03-16 06:19:03 -0700

    corrections

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,37 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table
- * 
- * n: parameter
- * 
+ * print_times_table - prints time table 
+ * @n: parameter
  * Return: (void)
  *
  */
-
 void print_times_table(int n)
 {
 	int x, y;
 
 	if (n <= 15 && n >= 0)
 	{
-	for (y = 0; y <= n; y++)
-	{
-		for (x = 0; x <= n; x++)
+		for (y = 0; y <= n; y++)
 		{
-			int product = x * y;
-			int []digits;
-
-			while (product)
+			for (x = 0; x <= n; x++)
 			{
-				int i = sizeof(digits) / sizeof(digits[0]);
-				products[i + 1] = product / 10;
-				product /= 10;
+				int product = x * y;
+				int products[500];
+				int digits[500];
+				while (product)
+				{
+					int i = sizeof(digits) / sizeof(digits[0]);
+					products[i + 1] = product / 10;
+					product /= 10;
+				}
+				for(int i = (sizeof(digits) / sizeof(digits[0]));i >= 0;i--)
+				{
+					putchar('0' + digits[i]);
+					putchar(',');	
+				}
 			}
-			for
-			(
-			int i = (sizeof(digits) / sizeof(digits[0]));
-			i >= 0;
-			i--
-			)
-			{
-				putchar('0' + digits[i]);
-				putchar(',');	
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }

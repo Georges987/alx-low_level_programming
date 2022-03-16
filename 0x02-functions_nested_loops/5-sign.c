9249a0ce1dd3bd28f71883c7f346577dde1872ad
Georges987 <ayenigeorgepierre@gmail.com>
2022-03-16 05:32:41 -0700

    Corrections

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,7 @@
 
 /**
  * print_sign - check if n is positive , negative or zero
- * 
- * alx : function parameter
- * 
+ * @alx : function parameter
  * Return: 1 if alx is positive, -1 if negative and 0 the other cases
  *
  */

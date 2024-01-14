7d9e2fae37adcad55276d5e56e05343a10895bed
Georges987 <ayenigeorgepierre@gmail.com>
2024-01-14 01:56:47 -0800

    writting strccat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strcat - function thact concatenate too string
+ *
+ * @dest: destination string
+ * @src: source string 
+ *
+ * Return: a pointer to dest
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int tail_dest = 0;
+	int tail_src = 0;
+
+	while (*(dest + tail_dest) != '\0')
+	{
+		tail_dest++;
+	}
+
+	while(tail_src >= 0)
+	{
+		*(dest + tail_dest) = *(src + tail_src)
+		if(*(src + tail_src) == '\0')
+			break;
+
+		tail_dest++;
+		tail_src++;
+	}
+
+	return dest;
+}
+
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
-# More pointer and string
+# lib
+
